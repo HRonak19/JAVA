7eb24f0f027c7d294730047156286fc682ad1ffc
RoankCODER19 <hedambharonak@gmail.com>
2025-05-28 10:04:08 -0400

    add switch statement

---
+++ TOPICS/4 Condition Statement/SWITCH/Enhanced Switch/Main.java
@@ -0,0 +1,17 @@
+class Main {
+
+    public static void main (String args[]) {
+    
+    int variable = 2;
+    
+    switch (variable) {
+
+        case 1 -> System.out.println("Something");
+        case 2 -> System.out.println("Another thing");
+
+    default -> System.out.println("Default case");
+    }
+
+
+
+}

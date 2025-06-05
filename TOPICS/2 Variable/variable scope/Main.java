2df4a6c60bc8c76dbdfc63a61a8e4d0fbba82b26
RoankCODER19 <hedambharonak@gmail.com>
2025-06-05 12:47:53 -0400

    new file

---
+++ TOPICS/2 Variable/variable scope/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+    static int x = 10; //class variable or scope variable
+
+        public static void main (String[] args) {
+
+            int y = 20; // local variable 
+
+                System.out.print(x);
+             System.out.print(y);
+        }
+}

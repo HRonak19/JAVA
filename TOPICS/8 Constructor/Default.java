6298ec81f6d0d684dd56cbedacb824b178be0625
[HRonak19] <[hedambharonak@gmail.com]>
2025-08-11 14:58:44 +0530

    new file

---
+++ TOPICS/8 Constructor/Default.java
@@ -0,0 +1,17 @@
+class bike {
+
+    bike() {
+
+        System.out.print("bike is avalabel ...");
+    }
+}
+
+class Default {
+
+    public static void main(String[] args) {
+
+            bike BK = new bike();
+
+            
+    }
+}
---
+++ TOPICS/8 Constructor/Parameterized.java
@@ -0,0 +1,27 @@
+class Student {
+
+    int rno;
+    String name;
+
+        Student(int r,String n) {
+
+            rno = r;
+            name = n;
+        }
+
+        void display () {
+
+            System.out.println(rno+" "+name);
+        }
+}
+
+class Parameterized {
+
+    public static void main (String[] args) {
+
+        Student ST = new Student(16,"Ronak");
+        Student ST1 = new Student(17,"Rohan");
+            ST.display();
+            ST1.display();
+    }
+}

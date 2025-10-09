faed3f58360d0d56d74b2876681f9d44eb032c4c
[HRonak19] <[hedambharonak@gmail.com]>
2025-10-09 17:14:36 +0530

    new file

---
+++ TOPICS/packge/add.java
@@ -0,0 +1,13 @@
+package arith;
+
+public class add {
+
+    public void a() {
+
+        int one=23,two=34;
+
+        int total = one + two;
+
+            System.out.print("total of = "+total);
+    }
+}
---
+++ TOPICS/packge/impo.java
@@ -0,0 +1,11 @@
+import arith.add;
+
+class impo {
+
+    public static void main (String args[]) {
+
+       add ad = new add();
+
+        ad.a();
+    }
+}

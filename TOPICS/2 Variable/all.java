40989194395c55cd48ad47797a2b243c97054d3a
[HRonak19] <[hedambharonak@gmail.com]>
2025-10-19 09:41:48 +0530

    cut file

---
+++ TOPICS/2 Variable/all.java
@@ -0,0 +1,14 @@
+class all {
+
+    public static void main (String []args) {
+
+        byte cont = 1;
+        short cont1 = 2;
+        int one = 1;
+        long two = 1000;
+        float three = 15.48;
+        double more = 12.10;
+        char ch = 'R';
+        Boolean what = "true"
+    }
+}
